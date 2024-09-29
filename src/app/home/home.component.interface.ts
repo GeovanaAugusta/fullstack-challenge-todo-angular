6e3db7a6df1f85acc8b2a6c037ce098f852a96df
geovanaaugusta <geovanaag@hotmail.com>
2024-09-29 16:50:56 -0300

    style: estilização pertinente

--- src/app/home/home.component.interface.ts
+++ src/app/home/home.component.interface.ts
@@ -11,7 +11,7 @@ export interface Task {
 }
 
 export interface User {
-  id: number,
+  id?: number,
   nome: string;
   telefone: string | null,
   email: string,
--- src/app/services/users-service.ts
+++ src/app/services/users-service.ts
@@ -12,7 +12,7 @@ export class UserService {
     constructor(private http: HttpClient) { }
 
     addUser(user: any): Observable<any> {
-        return this.http.post<any>(`${this.apiUrl}/`, user);
+        return this.http.post<any>(`${this.apiUrl}`, user);
     }
 
     allUsers(): Observable<any> {
